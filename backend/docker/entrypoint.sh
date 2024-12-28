f09d7e788d1780c0424c4ea568717abd8af7d0cc
prosabd <102315258+prosabd@users.noreply.github.com>
2024-12-28 14:39:29 +0100

    feat: :heavy_minus_sign: clean code + create db for server env

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,6 +1,7 @@
 set -e
 
-composer install
+# composer install
+php bin/console doctrine:database:create --if-not-exists
 php bin/console doctrine:migrations:migrate -n
 php bin/console doctrine:fixtures:load -n
 php bin/console lexik:jwt:generate-keypair --skip-if-exists --no-interaction
