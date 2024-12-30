85795e0bf8f37ffefb0b94181821f6751e3e8fa6
prosabd <102315258+prosabd@users.noreply.github.com>
2024-12-30 06:17:59 +0100

    fix: :sparkles: some elements fixes on dockerfile and entrypoint

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 set -e
 
-composer install
+composer install --prefer-dist --no-scripts --no-autoloader
 php bin/console doctrine:migrations:migrate -n
 php bin/console doctrine:fixtures:load -n
 php bin/console lexik:jwt:generate-keypair --skip-if-exists --no-interaction
