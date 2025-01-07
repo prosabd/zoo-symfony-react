fb97b1b497d45b91e88b5e6de5269f18e9b1773c
prosabd <102315258+prosabd@users.noreply.github.com>
2025-01-07 09:01:59 +0100

    feat: :heavy_plus_sign: add assets install on backend command docker config

--- backend/docker/entrypoint.sh
+++ backend/docker/entrypoint.sh
@@ -6,5 +6,7 @@ php bin/console doctrine:database:create --if-not-exists --connection=default
 php bin/console doctrine:migrations:migrate -n
 php bin/console doctrine:fixtures:load -n
 php bin/console lexik:jwt:generate-keypair --skip-if-exists --no-interaction
+php bin/console assets:install --symlink --relative public
+chown 1000:1000 -R public
 
 exec "$@"
